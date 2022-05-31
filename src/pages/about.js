44af0ec4c23a00b8df768caab1eba0cac4dd206a
ohyoungkyu <come8908@gmail.com>
2022-05-31 12:41:50 +0900

    about페이지 링크 추가 및 test 페이지 추가

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "gatsby";
 
 function AboutPage() {
   return (
     <main>
       <title>자기소개</title>
       <h1>자기소개 페이지 입니다.</h1>
+      <Link to="https://converse.co.kr/index.html">컨버스 카테고리</Link>
     </main>
   );
 }
