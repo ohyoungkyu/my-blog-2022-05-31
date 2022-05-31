3944ca2399e1f838c3279d7f20e1c763f86d879d
ohyoungkyu <come8908@gmail.com>
2022-05-31 12:54:21 +0900

    index페이지에 about,test,test-space 모두추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "gatsby";
 
 const IndexPage = () => {
   return (
@@ -6,7 +7,9 @@ const IndexPage = () => {
       <title>Home Page</title>
       <h1>Home Page</h1>
       <Link to="https://converse.co.kr/index.html"> 컨버스 카테고리 </Link>
-      <Link to="http://localhost:8000/test"> test </Link>
+      <Link to="/test"> test </Link>
+      <Link to="/about"> about </Link>
+      <Link to="/test-space"> test-space </Link>
     </main>
   );
 };
