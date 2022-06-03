be61e84fb0817dbd2f22b08531bd99c0604db9a6
ohyoungkyu <come8908@gmail.com>
2022-06-03 13:21:00 +0900

    태그 표시

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
@@ -7,13 +7,15 @@ import CodeBlock from "../../components/CodeBlock";
 import { MDXProvider } from "@mdx-js/react";
 
 const BlogPost = ({ data }) => {
+  const tags = data.mdx.frontmatter.tags;
+
   const image =
     data.mdx.frontmatter.hero_image &&
     getImage(data.mdx.frontmatter.hero_image);
 
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
-      <p>Posted: {data.mdx.frontmatter.date}</p>
+      <p>{data.mdx.frontmatter.date}</p>
       {image && (
         <>
           <GatsbyImage
@@ -24,6 +26,7 @@ const BlogPost = ({ data }) => {
             Photo Credit:{" "}
             <a
               target="_blank"
+              rel="noreferrer"
               href={data.mdx.frontmatter.hero_image_credit_link}
             >
               {data.mdx.frontmatter.hero_image_credit_text}
@@ -31,9 +34,7 @@ const BlogPost = ({ data }) => {
           </p>
         </>
       )}
-
       <hr />
-
       <MDXProvider
         components={{
           pre: CodeBlock,
@@ -41,14 +42,14 @@ const BlogPost = ({ data }) => {
       >
         <MDXRenderer>{data.mdx.body}</MDXRenderer>
       </MDXProvider>
+      <hr />
+      TAGS : {tags && tags.join(", ")}
     </Layout>
   );
 };
-
 export const query = graphql`
   query ($id: String) {
     mdx(id: { eq: $id }) {
-      body
       frontmatter {
         title
         date(formatString: "MMMM DD, YYYY")
@@ -60,9 +61,10 @@ export const query = graphql`
             gatsbyImageData
           }
         }
+        tags
       }
+      body
     }
   }
 `;
-
 export default BlogPost;
